7d31b54a534af23e93c37dfdca434a3a57c445c4
Hugo Meneses <hugo_meneses@usmp.pe>
2026-05-07 17:58:41 -0500

    rama hugo2

--- artifacts/admin/vite.config.ts
+++ artifacts/admin/vite.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
     },
     dedupe: ["react", "react-dom"],
   },
-  root: path.resolve(import.meta.dirnae),
+  root: path.resolve(import.meta.dirname),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
